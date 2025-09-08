c8a40ebf2a811509e12d005ba15785c2083a0a7b
Sangwoo <tkddn8828@hansung.ac.kr>
2025-09-09 00:56:55 +0900

    추fix : profile 문제 해결

--- src/main/java/com/moodTrip/spring/global/web/GlobalControllerAdvice.java
+++ src/main/java/com/moodTrip/spring/global/web/GlobalControllerAdvice.java
@@ -1,9 +1,11 @@
 package com.moodTrip.spring.global.web;
 
-
 import com.moodTrip.spring.domain.emotion.dto.response.EmotionCategoryDto;
 import com.moodTrip.spring.domain.emotion.service.EmotionService;
+import com.moodTrip.spring.domain.member.service.ProfileService;
+import com.moodTrip.spring.global.security.jwt.MyUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -14,11 +16,25 @@ import java.util.List;
 public class GlobalControllerAdvice {
 
     private final EmotionService emotionService;
+    private final ProfileService profileService;
 
-    // "headerEmotionCategories"라는 이름으로 모델에 데이터를 추가하는 메서드
-    // 모든 컨트롤러가 실행되기 전에 호출되어, 어느 페이지에서든 해당 데이터를 사용 가능
+    /**
+     * 헤더 감정 카테고리 공통 주입
+     */
     @ModelAttribute("headerEmotionCategories")
     public List<EmotionCategoryDto> addHeaderEmotionCategoriesToModel() {
         return emotionService.getEmotionCategories();
+    }
+
+    /**
+     * 로그인된 사용자의 프로필 이미지 공통 주입
+     */
+    @ModelAttribute("profileImage")
+    public String addProfileImageToModel(@AuthenticationPrincipal MyUserDetails userDetails) {
+        if (userDetails == null) return null;
+
+        Long memberPk = userDetails.getMember().getMemberPk();
+        // ProfileService 활용
+        return profileService.getProfileByMemberId(memberPk).getProfileImage();
     }
 }
