a213f9727fd05586a86b0149cb0440f2eb159278
Sangwoo <tkddn8828@hansung.ac.kr>
2025-09-18 15:26:12 +0900

    fix : 전체마지막 에러 수정

--- src/main/java/com/moodTrip/spring/global/config/RedisConfig.java
+++ src/main/java/com/moodTrip/spring/global/config/RedisConfig.java
@@ -4,6 +4,7 @@ import com.moodTrip.spring.domain.chat.service.RedisSubscriber;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.PatternTopic;
@@ -14,6 +15,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
+@Profile({"prod","local"})
 @RequiredArgsConstructor
 public class RedisConfig {
 
